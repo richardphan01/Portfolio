cc4a3ff8c767c6111d7d788bef94d8146f71c27c
Richard Phan <richard.phan01@gmail.com>
2021-09-12 15:58:24 -0400

    edited Topbar.jsx

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -20,7 +20,7 @@ export default function Topbar({menuOpen, setMenuOpen}) {
                 </div>
                 <div className="right">
                     <div className="itemContainer">
-                        <span><a href={Resume} target="_blank">Resume</a></span>
+                        <span><a href={Resume} target="blank">Resume</a></span>
                     </div>
                     <div className="hamburger" onClick={()=> setMenuOpen(!menuOpen)}>
                         <span className="line1"></span>
